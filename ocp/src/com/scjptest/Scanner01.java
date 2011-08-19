53fe9b5a270616190cb235ed99992e4fc556caef
LeoLuz <leonardo.la@gmail.com>
2011-08-19 18:16:03 -0300

    Scanner test added

---
+++ ocp/src/com/scjptest/Scanner01.java
@@ -0,0 +1,32 @@
+package com.scjptest;
+
+import java.util.Scanner;
+import java.io.Console;
+import java.util.regex.Pattern;
+
+public class Scanner01 {
+
+    private Scanner sc;
+    private String in;
+
+    public Scanner01(){
+    }
+
+    public void readConsole() {
+        Console cons = System.console();
+        this.in = cons.readLine();
+        this.sc = new Scanner(in);
+    }
+
+    public static void main (String[] args) {
+
+        Scanner01 scann = new Scanner01();
+        System.out.print("Type anything you want: ");
+        scann.readConsole();
+        Pattern p = scann.sc.delimiter();
+
+        while (scann.sc.hasNext()) {
+            System.out.println("Delimiter: " + p.pattern());
+        }
+    }
+}
